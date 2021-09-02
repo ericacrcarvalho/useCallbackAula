19919319876679034a56fc0b847da0cd85119299
Erica Carvalho <ericacrcarvalho@gmail.com>
2021-09-01 22:28:30 -0300

    completo

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const MyButton = ({ title, onPress }) => {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    useEffect(() => {
+      console.log("useEffect");
+    });
+
+    useEffect(() => {
+      console.log("useEffect onPress");
+    }, [onPress]);
+
+    return <Button
+      title={title}
+      onPress={onPress}
+    />
+  }
+
+  const App = () => {
+   
+    const [count, setCount] = useState(0);
+
+    const add = useCallback(() => {
+      console.log("Add");
+      setCount(count+1);
+    }, []);
+
+    return (
+      <View style={styles.container}>
+      <Text style={[styles.text, {fontWeight: 'bold'}]}>useCallback</Text>
+      <Text style={styles.text}>Contador: {count}</Text>
+      <MyButton
+        title="Adicionar"
+        onPress={add}
+      />
+      </View>
+    );
+  }
+
+  export default App;
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      fontSize: 18,
+      margin: 6,
+    }
+  });
